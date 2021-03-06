7551b78cbc4502cce602ae96be33da93d2b3b7b6
becolomochi <becolomochi@gmail.com>
2021-03-06 18:30:36 +0900

    トップでメモを読み込む実装。他ページも体裁を整える

--- app.rb
+++ app.rb
@@ -1,22 +1,54 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 set :views, settings.root + '/views/articles'
 
-get '/articles/*/edit' do |id|
-  erb :edit
+get '/articles' do
+  @page_title = 'memo-app'
+
+  # jsonを読み込み、ハッシュに変換する
+  file = 'articles.json'
+  File.open(file) do |line|
+    hash = JSON.load(line)
+    @articles = hash["articles"]
+  end
+
+  erb :index
 end
 
-get '/articles/*' do |id|
-  erb :show
+post '/articles' do
+  # TODO: jsonファイルにメモを書き込む実装
 end
 
 get '/articles/new' do
+  @page_title = 'メモを追加'
+
   erb :new
 end
 
-get '/articles' do
-  erb :index
+get '/articles/:id' do |id|
+  @page_title = 'メモの詳細'
+
+  @id = params[:id]
+
+  erb :show
+end
+
+delete '/articles/:id' do
+  # TODO: jsonファイルからメモを削除する実装
+end
+
+patch '/articles/:id' do
+  # TODO: jsonファイルにメモを編集する実装
+end
+
+get '/articles/:id/edit' do |id|
+  @page_title = 'メモを編集'
+
+  @id = params[:id]
+
+  erb :edit
 end
 
 # TODO: 404
